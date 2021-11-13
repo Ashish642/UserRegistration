45db9aae66536de3d509778bf820b37a3ad86689
pitu <ashishkumarvish.432@gmail.com>
2021-11-13 20:08:52 +0530

    CheckingFirst name is in format or not

--- src/com/blz/userregistration/UserDetails.java
+++ src/com/blz/userregistration/UserDetails.java
@@ -1,7 +1,18 @@
 package com.blz.userregistration;
+import java.util.Scanner;
 
-public class UserDetails {
+public class UserDetails extends Regex {
+    public static Scanner scan = new Scanner(System.in);
+    public static String first_name, last_name, mail_id, phone_number, password, check_mail;
+
+
     public static void main(String[] args) {
-        System.out.println("Welcome to user registration");
+        UserDetails details = new UserDetails();
+        details.firstname();
+
+    }
+
+    public static void firstname() {
+        System.out.println("\nEnter the first name with first latter capital and name should be minimum 3 letters: ");
     }
 }
