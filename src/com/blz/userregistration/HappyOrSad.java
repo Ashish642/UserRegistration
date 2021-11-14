8f252923a4e81556b759d47b8dee487c1b8cfeda
pitu <ashishkumarvish.432@gmail.com>
2021-11-14 15:53:38 +0530

    ValidatingDetails And mood analyzer

---
+++ src/com/blz/userregistration/HappyOrSad.java
@@ -0,0 +1,12 @@
+package com.blz.userregistration;
+
+
+public class HappyOrSad {
+    public static String moodAnalyze(String mood) {
+        if (mood.equals("HAPPY"))
+            return "HAPPY";
+            else
+                return "SAD";
+
+    }
+}
